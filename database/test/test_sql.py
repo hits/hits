363245e28aeaf8fa921a2a28305a66eea20d49d8
Matthew Rocklin <mrocklin@gmail.com>
2012-07-02 21:45:37 -0500

    fixed failing test in test_sql

--- database/test/test_sql.py
+++ database/test/test_sql.py
@@ -6,7 +6,7 @@ def test_stringify():
 def test_insert_statement():
     assert insert_statement({'name': 'joe'}, "NameTable") == \
             """INSERT INTO NameTable\n(name)\nVALUES\n("joe");"""
-    assert insert_statement({'name': 'joe', 'age': 14}, "NameTable") == \
+    assert insert_statement({'name': 'joe', 'age': '14'}, "NameTable") == \
             """INSERT INTO NameTable\n(age, name)\nVALUES\n("14", "joe");"""
 
 
