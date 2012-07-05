6388bc366303eec49ee280d9715523635b4204e9
John Jacobsen <john@mail.npxdesigns.com>
2012-07-05 23:32:26 +0000

    Demonstrate insert and query

--- src/hits/models/migration.clj
+++ src/hits/models/migration.clj
@@ -13,13 +13,23 @@
 
 (defn create-actions []
   (sql/with-connection (System/getenv "DATABASE_URL")
-    (when-not (some #{'actions} (table-names)))
-    (sql/create-table :actions
-                      [:id "varchar(40)"]
-                      [:action "varchar(4)"]
-                      [:path "varchar(256)"])))  ;; should be textfield?
+    (when-not (some #{"actions"} (table-names))
+      (sql/create-table :actions
+                        [:id "varchar(40)"]
+                        [:action "varchar(4)"]
+                        [:path "varchar(256)"]))))  ;; should be textfield?
+
+(defn test-action-insert []
+  (sql/with-connection (System/getenv "DATABASE_URL")
+    (sql/insert-record :actions {:id "0987135", :action "TEST", :path "/dev/null"})))
+
+(defn all-actions []
+  (sql/with-connection (System/getenv "DATABASE_URL")
+    (sql/with-query-results results ["select * from actions"] (into [] results))))
 
 (defn -main []
   (print "Migrating database...") (flush)
   (create-actions)
-    (println " done"))
+  (test-action-insert)
+  (println " done.  Table contents:")
+  (println (all-actions)))
