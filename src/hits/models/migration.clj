0114c1d680f698759322aa15e70d47480694b688
John Jacobsen <john@mail.npxdesigns.com>
2012-07-05 18:05:09 -0500

    Update to check table existence first

--- src/hits/models/migration.clj
+++ src/hits/models/migration.clj
@@ -1,8 +1,19 @@
 (ns hits.models.migration
   (:require [clojure.java.jdbc :as sql]))
 
+(defn db-get-tables []
+  (into []
+        (sql/resultset-seq
+         (-> (sql/connection)
+             (.getMetaData)
+             (.getTables nil nil nil (into-array ["TABLE" "VIEW"]))))))
+
+(defn table-names []
+  (map :table_name (db-get-tables)))
+
 (defn create-actions []
   (sql/with-connection (System/getenv "DATABASE_URL")
+    (when-not (some #{'actions} (table-names)))
     (sql/create-table :actions
                       [:id "varchar(40)"]
                       [:action "varchar(4)"]
