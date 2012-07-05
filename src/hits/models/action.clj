5a3f7cdd7e0c80f8568b397c14b0768f0d06e119
Matthew Rocklin <mrocklin@gmail.com>
2012-07-05 17:50:27 -0500

    added two standard queries to modesl/action.clj

--- src/hits/models/action.clj
+++ src/hits/models/action.clj
@@ -1,13 +1,34 @@
 (ns hits.models.action
   (:require [clojure.java.jdbc :as sql]))
 
-;; CRUD stuff here
+(defn hits-query [query]
+  (sql/with-connection (System/getenv "DATABASE_URL")
+    (sql/with-query-results results [query]
+      (into [] results))))
 
-;; (defn all []
-;;   (sql/with-connection (System/getenv "DATABASE_URL")
-;;     (sql/with-query-results results
-;;       ["select * from shouts order by id desc"]
-;;       (into [] results))))
+
+
+(defn files-touched-by-user [author-name]
+  (hits-query 
+    (format
+       "SELECT path, count(path) as cnt
+        FROM log INNER JOIN actions
+        ON log.id = actions.id
+        WHERE author_name = '%s'
+        GROUP BY path
+        ORDER BY cnt DESC" 
+       author-name)))
+
+(defn users-active-in-path [path]
+  (hits-query 
+    (format
+       "SELECT author_name, count(author_name) as cnt 
+        FROM log INNER JOIN actions
+        ON log.id = actions.id
+        WHERE path like '%s'
+        GROUP BY author_name 
+        ORDER BY cnt DESC"
+       path)))
 
 ;; (defn create [action]
 ;;   (sql/with-connection (System/getenv "DATABASE_URL")
