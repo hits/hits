6bbfafb9bc7ae60d2039f8b0cb77407c976dcbd6
Matthew Rocklin <mrocklin@gmail.com>
2012-07-06 17:13:39 -0500

    added gitparse
    This file copies over functionality from the python parsing solution
    
    It grabs data from the git command line, parses and pushes it into clojure maps

---
+++ src/hits/models/gitparse.clj
@@ -0,0 +1,66 @@
+(ns hits.models.gitparse
+  (:require [clojure.string :as string])
+  (:require [clojure.java.shell :as shell]))
+     
+
+;; git log 
+(def git-fields {"id" "%H"
+                 "author_name" "%an"
+                 "author_email" "%ae"
+                 "date" "%ad"
+                 "timestamp" "%at"
+                 "subject" "%s"
+                 "body" "%B"})
+
+(def line-terminator "\nline-terminator525724924752\n")
+(def msg-terminator  "\n\nmessage terminator525925825722825\n\n")
+
+(def log-format (str (string/join line-terminator (vals git-fields))
+                     msg-terminator))
+
+(defn log-msg-to-map [msg]
+  (zipmap (keys git-fields)
+          (string/split msg (re-pattern line-terminator))))
+
+(defn parse-log []
+  (let [text (:out (shell/sh "git" "log" (format "--format=%s" log-format)))
+        msgs (string/split text (re-pattern msg-terminator))]
+    (map log-msg-to-map msgs)))
+
+;; git whatchanged
+
+(def whatchanged-format (str msg-terminator "%H"))
+
+(defn parse-action-line [line]
+  (let [words  (string/split line #"\s+")
+        action (first words)
+        path   (string/join " " (rest words))]
+    [action path]))
+
+(defn non-trivial-line [line]
+  (not (= (string/trim line) "")))
+
+(defn wc-msg-to-map [msg]
+  (let [lines (string/split msg #"\n")
+        id (first lines)
+        action-lines (filter non-trivial-line (rest lines))
+        actions-and-paths (map parse-action-line action-lines)]
+    [id actions-and-paths]))
+
+(defn parse-whatchanged []
+  (let [text (:out (shell/sh "git" "whatchanged" 
+                             (format "--format=%s" whatchanged-format)
+                             "--name-status"))
+        msgs (string/split text (re-pattern msg-terminator))
+        good-msgs (filter non-trivial-line msgs)]
+    (map wc-msg-to-map good-msgs)))
+        
+(defn flatten-whatchanged-map [[id actions-and-paths]]
+  (map (fn [[action path]] {"id" id "action" action "path" path})
+       actions-and-paths))
+
+;; whatchanged testing
+(def id-appair (first (parse-whatchanged )))
+(def flat-map (flatten-whatchanged-map id-appair))
+(def final-result (map flatten-whatchanged-map (parse-whatchanged)))
+
