65e8010d1d690c7802e0381e2898b8be36b2ad69
Matthew Rocklin <mrocklin@gmail.com>
2012-09-09 10:18:43 -0500

    Clojure version 1.2.1 -> 1.4. Also add datomic

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject hits "0.0.1"
   :main hits.core
   :dependencies
-  [[org.clojure/clojure "1.2.1"]
+  [[org.clojure/clojure "1.4.0"]
    [compojure "0.6.4"]
    [expectations "1.3.3"]
    [hiccup "0.3.6"]
@@ -9,5 +9,6 @@
    [noir "1.2.2"]
    [org.clojure/java.jdbc "0.1.1"]
    [ring/ring-jetty-adapter "0.3.10"]
-   [clj-time "0.4.3"]]
+   [clj-time "0.4.3"]
+   [com.datomic/datomic-free "0.8.3479"]]
   :dev-dependencies [[lein-autoexpect "0.1.1"]])
