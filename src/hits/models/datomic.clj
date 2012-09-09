db235e557fdb7985359b86754e78461bd7fa5a35
Matthew Rocklin <mrocklin@gmail.com>
2012-09-09 18:06:59 -0500

    add starter datomic code

---
+++ src/hits/models/datomic.clj
@@ -0,0 +1,46 @@
+(ns hits.models.datomic
+  (:require [datomic.api :only [q db] :as d])
+  (:require [hits.models.git-schema :only [schema] :as git] )
+  (:require [hits.models.gitparse :only [parse-log parse-whatchanged] :as parse])
+  (:require [clj-time.format :as timef])
+  (:require [clj-time.coerce :as timec]))
+
+(def key-translate 
+  {"author_name"  :git/author-name
+   "author_email" :git/author-email
+   "id"           :git/id
+   "subject"      :git/subject
+   "body"         :git/body
+   "date"         :git/date
+   "timestamp"    :git/timestamp
+   "action"       :git.change/action
+   "file"         :git.change/file}
+   )
+
+(defn to-timestamp [s]
+  (->> s
+    (timef/parse (timef/formatter "EEE MMM dd HH:mm:ss yyyy Z"))
+    timec/to-timestamp))
+
+(defn parse-int [s]
+  (Integer. (re-find #"[0-9]*" s)))
+
+(def transformations 
+  [(defn timestamp [m]
+     (if (contains? m "timestamp")
+       (update-in m ["timestamp"] parse-int)
+        m))
+   (defn date [m]
+     (if (contains? m "date")
+       (update-in m ["date"] to-timestamp)
+        m))])
+
+
+(defn translate [logmap]
+  (let [oldkeys (keys logmap)
+        newkeys (map key-translate oldkeys)
+        newmap  (reduce (fn [m trns] (trns m)) logmap transformations)]
+    (zipmap newkeys (map newmap oldkeys))))
+
+(defn add-new-id [m]
+  (assoc m :db/id (datomic.api/tempid :db.part/user)))
---
+++ test/hits/models/datomic_test.clj
@@ -0,0 +1,61 @@
+(ns hits.models.datomic-test
+  (:use clojure.test
+        hits.models.datomic)
+  (:require [datomic.api :only [q db] :as d])
+  (:require [hits.models.git-schema :only [schema] :as git] )
+  (:require [hits.models.gitparse :only [parse-log parse-whatchanged] :as parse]))
+
+(deftest test-timestamp
+  (is (= (type (to-timestamp "Fri Jul 6 11:59:00 2012 -0500")) 
+         java.sql.Timestamp)))
+
+(defn set= [a b]
+  (= (set a) (set b)))
+
+(deftest test-translate
+  (let [logmap          {"id" "ABCD"
+                         "subject" "a subject" 
+                         "body" "a body" 
+                         "author_name" "Joe Schmoe" 
+                         "date" "Fri Jul 6 11:59:00 2012 -0500"
+                         "timestamp" "10000000"}
+        dtmmap          (translate logmap)
+        expected-dtmmap {:git/id "ABCD" 
+                         :git/subject "a subject" 
+                         :git/body "a body" 
+                         :git/author-name "Joe Schmoe" 
+                         :git/date (to-timestamp "Fri Jul 6 11:59:00 2012 -0500")
+                         :git/timestamp 10000000}]
+    (is (set= (keys expected-dtmmap) (keys dtmmap)))
+    (is (set= (vals expected-dtmmap) (vals dtmmap)))
+    (is (= (type (:git/date dtmmap)) java.sql.Timestamp))))
+
+(deftest test-add-new-id
+  (let [oldmap {:git/id "ABCD" :git/subject "a subject"}
+        newmap (add-new-id oldmap)]
+    (is (contains? newmap :db/id))))
+
+(def uri "datomic:mem://hits-test")
+(d/create-database uri)
+(def conn (d/connect uri))
+@(d/transact conn git/schema)
+(def data (map add-new-id (map translate (parse/parse-log "hits" "hits-test"))))
+@(d/transact conn data)
+
+(deftest test-query-subjects
+  (is (contains? (d/q '[:find ?sub :where [?c :git/subject ?sub]] (d/db conn)) ["First commit"])))
+
+(deftest test-author-of-first-commit
+  (is (= (ffirst (d/q '[:find ?name :where [?c :git/subject "First commit"]
+                                            [?c :git/author-name ?name]] (d/db conn)))
+         "Matthew Rocklin")))
+           
+    
+;(clojure.pprint/pprint (d/q '[:find ?name ?email :where [?c :git/author-email ?email] [?c :git/author-name ?name]] (d/db conn)))
+;(clojure.pprint/pprint (seq (d/q '[:find ?c ?name ?id :where [?c :git/id "804d5c62d429e313a542630e58efd3de6a0a1d02"] [?c :git/author-name ?name] [?c :git/id ?id]] (d/db conn))))
+
+(test-timestamp)
+(test-translate)
+(test-add-new-id)
+(test-query-subjects)
+(test-author-of-first-commit)
